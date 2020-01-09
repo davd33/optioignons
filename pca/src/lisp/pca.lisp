e06f3bf871c01db356e325edfe3f05d147e21055
David Rueda <davd33@gmail.com>
2020-01-09 14:02:49 +0100

    mito - db - connect

--- pca/src/lisp/pca.lisp
+++ pca/src/lisp/pca.lisp
@@ -1,3 +1,7 @@
 ;;; Principal Component Analysis
 
 (in-package #:pca)
+
+;; connect to database
+(mito:connect-toplevel :mysql :database-name "learn_stats" :username "postgres" :password "helloworld")
+

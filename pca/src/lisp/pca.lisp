bf6584549efb1340df504500eb05c80851f57da7
David Rueda <davd33@gmail.com>
2020-01-10 21:24:26 +0100

    pca - individuals similarity

--- pca/src/lisp/pca.lisp
+++ pca/src/lisp/pca.lisp
@@ -8,6 +8,7 @@
                        :username "postgres"
                        :password "helloworld")
 
+;; PCA DATABASE STUFF
 (mito:deftable pca ()
   ((city :col-type (:varchar 64))
    (january :col-type (:real))
@@ -26,6 +27,14 @@
    (latitude :col-type (:real)))
   (:unique-keys city))
 
+(defun pca-obj? (pca)
+  "Return T if pca is a pca object."
+  (eq 'pca (type-of pca)))
+
+(defun not-pca-obj? (pca)
+  "Complements pca-obj?"
+  (complement #'pca-obj?))
+
 (defun print-pca (pca)
   (format t "~&++ PCA ++")
   (format t "~& city = ~A" (slot-value pca 'city)))
@@ -34,6 +43,18 @@
   "Creates the table as defined in the mito:deftable call of the symbol table-type."
   (mapc #'mito:execute-sql (mito:table-definition table-type))
   (mito:ensure-table-exists table-type))
+
+;; PCA ALGORITHMS
+
+(defun pca ())
+
+(defun similarity (ind1 ind2 variables)
+  "Computes Pythagore similarity between two PCA objects.
+Variables are a list of symbols, as accessor of PCA objects."
+  (sqrt (loop for k in variables
+              sum (expt (- (slot-value ind1 k)
+                           (slot-value ind2 k))
+                        2))))
 
 (defun main ()
   (create-table 'pca)
